d376611982179c62aac31babf4af6fd4fc4660da
zoumo <jim.zoumo@gmail.com>
2020-08-05 23:47:26 +0800

    feat: add pointer for built-in types

---
+++ pointer/builtin.go
@@ -0,0 +1,65 @@
+package pointer
+
+func Bool(t bool) *bool {
+	return &t
+}
+
+func Int(i int) *int {
+	return &i
+}
+
+func Int8(i int8) *int8 {
+	return &i
+}
+
+func Int16(i int16) *int16 {
+	return &i
+}
+
+func Int32(i int32) *int32 {
+	return &i
+}
+
+func Int64(i int64) *int64 {
+	return &i
+}
+
+func Uint(i uint) *uint {
+	return &i
+}
+
+func Uint8(i uint8) *uint8 {
+	return &i
+}
+
+func Uint16(i uint16) *uint16 {
+	return &i
+}
+
+func Uint32(i uint32) *uint32 {
+	return &i
+}
+
+func Uint64(i uint64) *uint64 {
+	return &i
+}
+
+func Float32(f float32) *float32 {
+	return &f
+}
+
+func Float64(f float64) *float64 {
+	return &f
+}
+
+func String(s string) *string {
+	return &s
+}
+
+func Complex64(c complex64) *complex64 {
+	return &c
+}
+
+func Complex128(c complex128) *complex128 {
+	return &c
+}
